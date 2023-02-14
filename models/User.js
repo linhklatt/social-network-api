f28f7d2dfc7303cd66ae0d7b4f8927d9637739c0
linhklatt <112294288+linhklatt@users.noreply.github.com>
2023-02-13 20:43:40 -0800

    created model for User and Thought

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      match: [
+        /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+        "Not a valid email!",
+      ],
+    },
+    thought: {
+      type: Schema.Types.ObjectId,
+      ref: "thought",
+    },
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("user, userSchema");
+
+module.exports = User;
